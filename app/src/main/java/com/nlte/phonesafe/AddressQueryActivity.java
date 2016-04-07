2a9571be8eff158742cb5b6a3d1cd30bb063e117
NLTE <HGQ577068802@qq.com>
2016-04-07 13:54:28 +0800

    文本输入框抖动效果实现

--- app/src/main/java/com/nlte/phonesafe/AddressQueryActivity.java
+++ app/src/main/java/com/nlte/phonesafe/AddressQueryActivity.java
@@ -9,6 +9,8 @@ import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -56,9 +58,14 @@ public class AddressQueryActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String num  = inputNumEt.getText().toString().trim();
-                String location = AddressDao.queryAdress(context, num);
-                if (!TextUtils.isEmpty(num)){
-                    locationTv.setText(location);
+                if (TextUtils.isEmpty(num)){
+                    Animation shake = AnimationUtils.loadAnimation(context, R.anim.shake);
+                    inputNumEt.startAnimation(shake);
+                }else{
+                    String location = AddressDao.queryAdress(context, num);
+                    if (!TextUtils.isEmpty(num)){
+                        locationTv.setText(location);
+                    }
                 }
             }
         });
