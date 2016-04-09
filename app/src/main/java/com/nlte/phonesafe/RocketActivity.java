e6a6ec09b7c39bc60214ee83c1931f13ccfa6855
NLTE <HGQ577068802@qq.com>
2016-04-09 16:35:13 +0800

    增加 ：点击小火箭时的手机震动功能

--- app/src/main/java/com/nlte/phonesafe/RocketActivity.java
+++ app/src/main/java/com/nlte/phonesafe/RocketActivity.java
@@ -1,10 +1,12 @@
 package com.nlte.phonesafe;
 
+import android.content.Context;
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -16,6 +18,7 @@ public class RocketActivity extends AppCompatActivity {
     private PointF startF;//一个点的位置，包括X， Y
     private Matrix matrix = new Matrix();//矩阵
     private Matrix startMatrix = new Matrix();//图片的起始矩阵
+    private Vibrator vibrator;
     private Handler handle = new Handler(){
         @Override
         public void handleMessage(Message msg) {
@@ -29,6 +32,8 @@ public class RocketActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rocket);
+        //获取系统的震动功能
+        vibrator  = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
         rocketIv = (ImageView) findViewById(R.id.rocket_iv);
         //对小火箭的触摸监听事件
         rocketIv.setOnTouchListener(new View.OnTouchListener() {
@@ -36,6 +41,8 @@ public class RocketActivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()){
                     case MotionEvent.ACTION_DOWN://一根手指按下:
+                        //震动100ms
+                        vibrator.vibrate(100);
                         //获取手指按下后的点的位置
                         startF = new PointF(event.getX(), event.getY());
                         //获取当前图片的矩阵
@@ -50,7 +57,9 @@ public class RocketActivity extends AppCompatActivity {
                         //矩阵的平移
                         matrix.postTranslate(dx, dy);
                         break;
-                    case MotionEvent.ACTION_UP://手指松开
+                    case MotionEvent.ACTION_UP://手指松开:
+                        //取消震动
+                        vibrator.cancel();
                         /*小火箭实现发射*/
                         rockShot();
                         break;
